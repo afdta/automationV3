dc3ec375b341c77cf372207d1845ca0942c29722
Alec Friedhoff <afriedhoff@gmail.com>
2018-12-19 15:35:13 -0500

    picking back up. currently this is just a toy model of a scrollytelling framework

--- build/js/main.js
+++ build/js/main.js
@@ -1,6 +1,6 @@
 import dir from "../../../js-modules/rackspace.js";
 import degradation from "../../../js-modules/degradation.js";
-
+import scroll_and_tell from '../../../js-modules/scroll-and-tell.js';
 
 //main function
 function main(){
@@ -17,10 +17,17 @@ function main(){
   //dir.add("dirAlias", "rackspace-slug/path/to/dir");
   var compat = degradation(document.getElementById("metro-interactive"));
 
+  var cback = function(ratio, forward){
+    //console.log(this);
+    console.log(ratio + " (" + this.target.id + " forward/down: " + forward + ")" )
+  }
+
 
   //browser degradation
   if(compat.browser()){
-    //run app...
+    //scroll_and_tell(document.getElementById("yellow"), cback).threshold([0.5]).margin(-50, 0, 0, 0, "%");
+    scroll_and_tell(document.getElementById("blue"), cback);
+    //scroll_and_tell(document.getElementById("yellow"), cback);
   }
 
 
